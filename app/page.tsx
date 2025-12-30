44e563930f2b3f39acf8bfca8404add6b8fcd636
Mauricio <mauricio1106007@gmail.com>
2025-12-29 22:57:15 -0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const aulas2:Aulas [] = [
 export default function Home() {
   return (
       <ContainerPrinc>
-      <div className="  min-h-screen flex w-full h-full justify-center items-center  md:w-[40%]  md:justify-end md:pr-6 bg-gradient-to-b from-[#5694D8] to-[#9966FF]">
+      <div className="  min-h-screen flex w-full h-full justify-center items-center   md:w-[40%]  md:justify-end md:pr-20 bg-gradient-to-b from-[#5694D8] to-[#9966FF]">
     <Image
       src="indigo.svg"
       width={100}
@@ -34,7 +34,7 @@ export default function Home() {
     />
   </div>
         <div className="  min-h-screen flex      justify-center items-center w-full bg-[#110F1C] h-full md:w-[60%] md:justify-start">
-    <div className=" pr-16 pl-16  flex mt-8 flex-col md:pr-0 md:pl-0  md: md:mt-0 md:justify-normal md:ml-24 h-full">
+    <div className=" pr-16 pl-16  flex mt-8 flex-col md:pr-0 md:pl-0  md: md:mt-0 md:justify-center md:ml-24 h-full">
           <Introduction></Introduction>
           <Checkboxs titulo1="AULAS NO SÁBADO | 14.09" aulas1={aulas} titulo2="AULAS NO DOMINGO | 15.09" aulas2={aulas2}></Checkboxs>
           <Payment></Payment>
